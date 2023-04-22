21f1cf19eb54d1d83128042f1c0fc1cfbabc8b34
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-04-22 12:45:13 -0300

    separe el scss y lo importe para que sea vea mas prolijo

--- javascript/animacion.js
+++ javascript/animacion.js
@@ -1,4 +1,9 @@
-function scrollToSection() {
+function scrollToSectionAbout() {
   const featuresSection = document.querySelector('.about-us');
+  featuresSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToSectionReferencias() {
+  const featuresSection = document.querySelector('.charts');
   featuresSection.scrollIntoView({ behavior: 'smooth' });
 }
