dd62d7924c9cbe43da98850b2a49fcb4e72ea23b
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-04-30 18:12:02 -0300

    Hice el responsive del Hero, e  Intro, ademas de ajustar pequeños errores

--- javascript/animacion.js
+++ javascript/animacion.js
@@ -1,14 +1,26 @@
+
+
+
+//SCROLL ABOUT US
 function scrollToSectionAbout() {
   const featuresSection = document.querySelector('.about-us');
   featuresSection.scrollIntoView({ behavior: 'smooth' });
 }
 
-function scrollToSectionReferencias() {
-  const featuresSection = document.querySelector('.charts');
+//SCROLL BENCHMARKING
+function scrollToSectionIntro() {
+  const featuresSection = document.querySelector('.intro');
   featuresSection.scrollIntoView({ behavior: 'smooth' });
 }
 
-function scrollToSectionReferencias() {
+//SCROLL BENCHMARKING
+function scrollToSectionBenchmarking() {
   const featuresSection = document.querySelector('.benchmarking');
+  featuresSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+//SCROLL CHARTS
+function scrollToSectionReferencias() {
+  const featuresSection = document.querySelector('.charts');
   featuresSection.scrollIntoView({ behavior: 'smooth' });
 }
